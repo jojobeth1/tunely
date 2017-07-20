443cd1b329412515c32e33e89dd9fb4e8bac2805
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-20 12:06:16 -0700

    set up controllers

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require('express'),
 
 app.use(express.static('public'));
 
+var db = require('./models');
+
+var controllers = require('./controllers');
+
+app.get('/api', controllers.api.index);
+
 
   // ROUTES
-
   app.get('/', function homepage (req, res){
     res.sendFile('views/index.html', {root: __dirname});
   });
