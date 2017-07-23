5f4de4806b976ef7f2c53fd6dad0a0be9bfd4bc6
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-23 09:15:59 -0700

    controllers for routes added

--- server.js
+++ server.js
@@ -5,24 +5,27 @@ app.use(express.static('public'));
 
 var db = require('./models');
 
-//var controllers = require('./controllers');
-
-//app.get('/api', controllers.api.index);
+var controllers = require('./controllers');
 
 
-  // ROUTES
-//  app.get('/', function(req, res){
-//   res.send('hello world');
-//  });
+/* *********
+  ROUTES
+********* */
 
-  app.get('/', function homepage (req, res){
-    res.sendFile('views/index.html', {root: __dirname});
-    console.log(__dirname);
-  });
+//API json view index
+app.get('/api', controllers.api.index);
+
+//Returns index page
+app.get('/', function homepage (req, res){
+  res.sendFile('views/index.html', {root: __dirname});
+  console.log(__dirname);
+});
 
 
 
-
+/* *********
+ LISTENING
+********* */
 app.listen(process.env.PORT || 3000, function(){
   console.log('Express server is running on localhost: 3000/ is working');
 });
